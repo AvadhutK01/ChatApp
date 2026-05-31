1abc7fca1303f61bcbc0dc51012e14683dcc6342
Avadhut Kelaskar <143504930+AvadhutK01@users.noreply.github.com>
2026-05-31 20:47:46 +0530

    Enhance Sequelize connection with SSL options
    Added SSL options for secure database connection.

--- BackEnd/dbConnect.js
+++ BackEnd/dbConnect.js
@@ -1,3 +1,19 @@
 const { Sequelize } = require("sequelize");
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_CONNECTION_USER, process.env.DB_CONNECTION_PASSWORD, { dialect: "mysql", host: process.env.DB_HOST })
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_CONNECTION_USER,
+  process.env.DB_CONNECTION_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+    dialectOptions: {
+      ssl: {
+        minVersion: "TLSv1.2",
+        rejectUnauthorized: true,
+      },
+    },
+  }
+);
+
 module.exports = sequelize;
